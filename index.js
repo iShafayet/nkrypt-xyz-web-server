810ed6a9bf1f51aa638ac4ae28185a9d97f7b34a
Sayem Shafayet <ishafayet@gmail.com>
2021-01-04 02:41:23 +0600

    Added nodejs http library based api routing and user-login api

---
+++ index.js
@@ -0,0 +1,75 @@
+
+const config = {
+  port: process.env.PORT || 9003,
+  dataDir: './data',
+  ssl: false
+};
+
+const reservedNodeKeys = {
+  rootNodeKey: "root".padStart(32, 0),
+  nullNodeKey: "".padStart(32, 0),
+};
+
+import * as httplib from 'http';
+import * as httpslib from 'https';
+
+import { parseRequestBody, sendJsonResponse, sendCaughtError, writeLog } from './lib/api-base.js';
+import { CodedError } from './lib/coded-error.js';
+import { AuthService } from './lib/auth-service.js';
+import { FileStorage } from './lib/file-storage.js';
+import { splitStringIntoChunks, bufferToString } from './lib/utils.js';
+
+let fileStorage = new FileStorage();
+await fileStorage.init({ dataDir: config.dataDir });
+
+let authService = new AuthService();
+await authService.init({ dataDir: config.dataDir });
+
+let weblib = (config.ssl ? httpslib : httplib);
+let server = weblib.createServer(async (req, res) => {
+  try {
+    // enable CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if (req.method === 'OPTIONS') {
+      res.writeHead(200);
+      res.end('OK. CORS is allowed.');
+      return;
+    }
+
+    // reject invalid protocols
+    if (req.method !== 'POST') {
+      res.writeHead(403);
+      res.end("Invalid protocol.");
+      return;
+    }
+
+    // parse url
+    let url = new URL(req.url, 'https://nkyrpt.xyz/');
+
+    // API /api/user-login
+    if (url.pathname === '/api/user-login') {
+      let body = await parseRequestBody(req);
+      let { username, password } = await JSON.parse(body);
+      // TODO: validate
+      let userData = await authService.login({ username, password });
+      return sendJsonResponse(res, userData);
+    }
+
+    
+
+    else {
+      res.writeHead(404);
+      res.end("Invalid API.");
+      return;
+    }
+
+  } catch (error) {
+    console.error(error);
+    sendCaughtError(res, error);
+  }
+});
+
+server.listen(config.port, () => {
+  writeLog(`Listening on port ${config.port}`);
+});

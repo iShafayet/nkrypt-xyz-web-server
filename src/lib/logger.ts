c9b2b43d2e999b89ddcf9034451c873c37bcfd0e
Sayem Shafayet <ishafayet@gmail.com~>
2022-08-15 23:08:55 +0600

    Color the output of logger.debug

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,3 +1,7 @@
+const STYLE = {
+  FgYellow: "\x1b[33m"
+};
+
 class Logger {
   private switches = {
     debug: true,
@@ -34,7 +38,7 @@ class Logger {
 
   debug(...args: any) {
     if (!this.switches.debug) return;
-    console.log.apply(console, ["DEBUG\t", ...args]);
+    console.log.apply(console, [STYLE.FgYellow, "DEBUG\t", ...args]);
   }
 
   log(...args: any) {

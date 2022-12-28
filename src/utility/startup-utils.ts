a1f0f71d9058196055b3fda51fd4e5a806d27413
Sayem Shafayet <ishafayet@gmail.com>
2022-12-28 23:06:28 +0600

    Add multi source config loader

--- src/utility/startup-utils.ts
+++ src/utility/startup-utils.ts
@@ -6,8 +6,8 @@ export const extractProcessParams = () => {
     throw new Error("Invalid number of arguments");
   }
 
-  let index = process.argv.findIndex(arg => {
-    return (arg.indexOf('start.js') > -1) || (arg.indexOf('start-dev.js') > -1)
+  let index = process.argv.findIndex((arg) => {
+    return arg.indexOf("start.js") > -1 || arg.indexOf("start-dev.js") > -1;
   });
 
   if (index === -1) {
@@ -15,9 +15,68 @@ export const extractProcessParams = () => {
   }
 
   return process.argv.slice(index + 1);
-}
+};
 
 export const loadConfig = (path: String): Config => {
-  let content = readFileSync(<any>path, { encoding: 'utf8' });
+  console.log(`STARTUP trying to load configuration from: ${path}`);
+  let content = readFileSync(<any>path, { encoding: "utf8" });
+  console.log(`STARTUP loading config: ${content}`);
   return JSON.parse(content);
-}
+};
+
+import { homedir } from "os";
+import path from "path";
+import os from "os";
+import constants from "./../constant/common-constants.js";
+
+const ARG_CONFIG_LOCATION = "--config";
+const ENVIRONMENT_CONFIG_LOCATION_KEY = "NK_CONFIG_LOCATION";
+const WINDOWS_PLATFORM_IDENTIFIER = "win32";
+const WINDOWS_PROGRAMDATA_FALLBACK = "C:\\ProgramData";
+
+export const lookupAndLoadConfigAsync = (commandLineParams: string[]) => {
+  // First priority is the command line parameter;
+  if (commandLineParams.indexOf(ARG_CONFIG_LOCATION) > -1) {
+    let index = commandLineParams.indexOf(ARG_CONFIG_LOCATION) + 1;
+    let configLocation = commandLineParams[index];
+
+    console.log(
+      "STARTUP Config location (from command line): ",
+      configLocation
+    );
+    return loadConfig(configLocation);
+  }
+
+  // Next priority is the environment variable;
+  if (process.env[ENVIRONMENT_CONFIG_LOCATION_KEY]) {
+    let configLocation = process.env[ENVIRONMENT_CONFIG_LOCATION_KEY];
+    console.log(
+      "STARTUP Config location (from environment variable): ",
+      configLocation
+    );
+    return loadConfig(configLocation);
+  }
+
+  // Next is homedir
+  try {
+    let configLocation = path.join(
+      homedir(),
+      constants.config.CONFIG_DIRECTORY_NAME,
+      constants.config.CONFIG_FILE_NAME
+    );
+    return loadConfig(configLocation);
+  } catch (ex) {
+    // Finally try /etc/nkrypt-xyz/config.json on linux and mac or ProgramData on windows
+    let etcDir =
+      os.platform() === WINDOWS_PLATFORM_IDENTIFIER
+        ? process.env.ALLUSERSPROFILE || WINDOWS_PROGRAMDATA_FALLBACK
+        : "/etc/";
+
+    let configLocation = path.join(
+      etcDir,
+      constants.config.CONFIG_DIRECTORY_NAME,
+      constants.config.CONFIG_FILE_NAME
+    );
+    return loadConfig(configLocation);
+  }
+};
